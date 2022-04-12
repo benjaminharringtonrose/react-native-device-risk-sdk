a563e57d47343e7efba99d01a809c90de042b52b
Benjamin Rose <Benjamin.Rose@apiture.com>
2022-04-12 09:28:33 -0400

    add frameworkl

--- ios/DeviceRiskSdk.m
+++ ios/DeviceRiskSdk.m
@@ -1,19 +1,19 @@
 #import "DeviceRiskSdk.h"
+@import FraudForce;
 
 @implementation DeviceRiskSdk
 
 RCT_EXPORT_MODULE()
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
+RCT_EXPORT_METHOD(getBlackboxId:(RCTResponseSenderBlock)callback){
+ @try{
+     NSString *blackbox = [FraudForce blackbox];
+   callback(@[[NSNull null], blackbox]);
+ }
+ @catch(NSException *exception){
+   callback(@[exception.reason, [NSNull null]]);
+ }
 }
 
 @end
+
